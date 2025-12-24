69971de1c565e618b1555520658a44135531140b
Amara Lavanya <119551895+LavanyaAmara@users.noreply.github.com>
2025-12-24 11:41:14 +0530

    Create left_rotate_array.cpp

---
+++ left_rotate_array.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter size of array:";
+    cin>>n;
+    vector<int> arr;
+    int x;
+    cout<<"Enter elements:";
+    for(int i=0;i<n;i++)
+    {
+        cin>>x;
+        arr.push_back(x);
+    }
+   //rotate an array by k steps
+   cout<<"Enter k : ";
+   int k;
+   cin>>k;
+  reverse(arr.begin(),arr.end()-k);
+  reverse(arr.end()-k,arr.end());
+  reverse(arr.begin(),arr.end());
+
+   //print new array
+   cout<<"New array: ";
+   for(int num:arr)
+   {
+       cout<<num<<" ";
+   }
+    return 0;
+}
